1fd66f3676e92744b87fc27d0cf13b51a7b0ddcd
SebassNoob <sebastian.ong@hotmail.com>
2024-03-24 02:54:40 +0800

    fix: cleanup on exit

--- impakt-backend/api/app.py
+++ impakt-backend/api/app.py
@@ -7,6 +7,8 @@ from quart_cors import cors
 from quart_schema import QuartSchema
 import os
 import sys
+from datetime import datetime
+import atexit
 
 # since this is the entrypoint for the application
 # we need to add the current working directory to the path
@@ -16,6 +18,7 @@ sys.path.append(os.getcwd())
 # load the environment variables
 load_dynamic_env()
 
+# create the app
 app = Quart(__name__)
 app = cors(app, allow_origin=os.getenv("API_URL", None))
 
@@ -23,6 +26,7 @@ app = cors(app, allow_origin=os.getenv("API_URL", None))
 QuartSchema(app)
 
 
+# handle errors manually thrown by the application
 @app.errorhandler(AppException)
 def handle_error(error: AppException):
     response = error.to_dict()
@@ -31,6 +35,7 @@ def handle_error(error: AppException):
     return response, error.status_code
 
 
+# handle errors thrown by the request schema validation
 @app.errorhandler(RequestSchemaValidationError)
 async def handle_request_validation_error(error: RequestSchemaValidationError):
 
@@ -39,6 +44,21 @@ async def handle_request_validation_error(error: RequestSchemaValidationError):
     return response, response.get("status_code")
 
 
+# register the blueprints
+# this is a map of routes to blueprints
+to_register = {"/hello": hello_blueprint}
+
+for route, blueprint in to_register.items():
+    app.register_blueprint(blueprint, url_prefix=route)
+
+
+# register on exit cleanup
+def cleanup():
+    app.logger.info(f"Shutting down at time: {datetime.now().isoformat()}")
+
+
+atexit.register(cleanup)
+
+# run the app
 if __name__ == "__main__":
-    app.register_blueprint(hello_blueprint, url_prefix="/hello")
     app.run(host="0.0.0.0", port=8080, debug=os.getenv("PYTHON_ENV", False))
