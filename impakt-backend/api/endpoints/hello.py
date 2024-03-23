20f73fcbf4efb5cb2123a76f7f9fc50f92febc62
SebassNoob <sebastian.ong@hotmail.com>
2024-03-24 02:36:46 +0800

    feat: handle validation errs

--- impakt-backend/api/endpoints/hello.py
+++ impakt-backend/api/endpoints/hello.py
@@ -1,17 +1,21 @@
 from quart import Blueprint, Request
-from quart_schema import validate_response, validate_request
-from pydantic import BaseModel
-import os
+from quart_schema import validate_response, validate_querystring, validate_request
+
 from models.tables import Company
+from dataclasses import dataclass
+
 
 hello_blueprint = Blueprint("hello", __name__)
 
 
-class HelloResponse(BaseModel):
-    name: str
+@dataclass
+class HelloResponse:
+    message: str
+    a: int
 
 
 @hello_blueprint.route("/", methods=["GET"])
-async def get_hello():
+@validate_querystring(HelloResponse)
+async def get_hello(query_args: HelloResponse):
     ret = await Company.test()
     return str(ret)
